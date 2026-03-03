86b973233400de9cb3828cb7b152379dc5f2da5c
Dani <daniguinotcar@gmail.com>
2026-03-03 09:18:49 +0100

    feat: crear clase Reina.java

---
+++ src/main/java/modelo/Reina.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class Reina extends Pieza{
+
+    public Reina(Color color, String dibujo, int fila, int columna, int puntos) {
+        super(color, dibujo, fila, columna, puntos);
+    }
+
+    @Override
+    public void mover() {
+
+    }
+
+}
