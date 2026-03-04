d03147a31f2f53f86d0a6e92fd5001dedd74e8b8
Dani <daniguinotcar@gmail.com>
2026-03-04 12:34:41 +0100

    feat: correccion metodo mover y implementacion del metodo comprobarMovimiento

--- src/main/java/modelo/Peon.java
+++ src/main/java/modelo/Peon.java
@@ -2,33 +2,34 @@ package modelo;
 
 public class Peon extends Pieza {
     private boolean primerMovimiento = true;
-
+    private int contador = 0;
     public Peon(Color color, String dibujo, int fila, int columna, int puntos) {
         super(color, dibujo, fila, columna, puntos);
     }
 
     @Override
     public void mover(int nuevaFila, int nuevaColumna) {
-        int distanciaFila = nuevaFila - this.getFila();
-        int distanciaColum = nuevaColumna - this.getColumna();
+        if (comprobarMovimiento(nuevaFila,nuevaColumna)){
+            this.setColumna(nuevaColumna);
+            this.setFila(nuevaFila);
+        }
+    }
 
+    @Override
+    public boolean comprobarMovimiento(int fila, int columna) {
+        int distanciaFila = getFila() - fila;
+        int distanciaColum = getColumna() - columna;
         boolean legal = false;
 
-        if (this.getColor() == Color.Blanca) {
-            if (distanciaFila == 1 & distanciaColum == 0) {
+        if (this.getColor() == Color.Blanca){
+            if (distanciaFila == 1 & distanciaColum == 0){
                 legal = true;
             }
-        } else {
-            if (distanciaFila == -1 & distanciaColum == 0) {
+        }else {
+            if (distanciaFila == -1 & distanciaColum == 0){
                 legal = true;
             }
         }
-
-        if (legal){
-            this.setFila(nuevaFila);
-            this.setColumna(nuevaColumna);
-        } else {
-            System.out.println("El movimiento es invalido");
-        }
+        return legal;
     }
 }
