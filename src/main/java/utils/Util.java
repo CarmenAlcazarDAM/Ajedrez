39034ba301f81de7726d79f1b33b752ed5e3d599
Dani <daniguinotcar@gmail.com>
2026-03-04 14:47:54 +0100

    feat: Metodos de utils basicos

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -1,4 +1,55 @@
 package utils;
 
-public class Util {
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public  class Util {
+    public static Scanner teclado = new Scanner(System.in);
+
+
+    public static int pideEntero(String msn) {
+        int n = 0;
+        boolean error = false;
+        do {
+            try {
+                System.out.println(msn);
+                n = teclado.nextInt();
+                error = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Valor no válido");
+                error = true;
+                teclado.next();
+            }
+        } while (error);
+        return n;
+    }
+    public static double pideDouble(String msn) {
+        double n = 0;
+        boolean error = false;
+        do {
+            try {
+                System.out.println(msn);
+                n = teclado.nextDouble();
+                error = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Valor no válido");
+                error = true;
+                teclado.next();
+            }
+        } while (error);
+        return n;
+    }
+
+    public static String pideString(String msn) {
+        String cadena=null;
+        System.out.println(msn);
+        cadena = teclado.next();
+        return cadena;
+    }
+
+    public static int generateRandomNumber(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
 }
