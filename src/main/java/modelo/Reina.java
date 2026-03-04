75c37b7bca573ba35558d531d50db41b5f7d32f1
Dani <daniguinotcar@gmail.com>
2026-03-04 12:57:59 +0100

    feat: implementando metodos mover y comprobarMovimientos

--- src/main/java/modelo/Reina.java
+++ src/main/java/modelo/Reina.java
@@ -6,17 +6,34 @@ public class Reina extends Pieza{
         super(color, dibujo, fila, columna, puntos);
     }
 
-    @Override
-    public boolean comprobarMovimiento(int fila, int columna) {
-        return false;
-    }
 
     @Override
     public void mover(int nuevaFila, int nuevaColumna) {
-        int distanciaFila = nuevaFila - this.getFila();
-        int distanciaColum = nuevaColumna - this.getColumna();
-
-
+        if (this.comprobarMovimiento(nuevaFila, nuevaColumna)){
+            this.setFila(nuevaFila);
+            this.setColumna(nuevaColumna);
+        }
         
+    }
+    @Override
+    public boolean comprobarMovimiento(int fila, int columna) {
+        int distanciaFila = getFila() - fila;
+        int distanciaColum = getColumna() - columna;
+        boolean legal = false;
+
+        if (distanciaFila < 0){
+            distanciaFila = distanciaFila * -1;
+        }
+        if (distanciaColum < 0){
+            distanciaColum = distanciaColum * -1;
+        }
+
+        if (distanciaFila == distanciaColum){
+            legal = true;
+        }
+        if (distanciaColum == 0 || distanciaFila == 0){
+            legal = true;
+        }
+        return legal;
     }
 }
