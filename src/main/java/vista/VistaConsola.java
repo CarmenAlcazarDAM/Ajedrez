6c504953a9153917097679069247196df47819dc
Kleopatra Raynova <kleo.rayn@gmail.com>
2026-03-05 13:49:57 +0100

    vistaTablero

--- src/main/java/vista/VistaConsola.java
+++ src/main/java/vista/VistaConsola.java
@@ -1,4 +1,37 @@
 package vista;
 
 public class VistaConsola {
-}
+    static final int FILA = 8;
+    static final int COLUMNA = 8;
+
+    /**
+     * Imprime el tablero línea a línea basado en el número de fila y lieas.
+     * @param tablero
+     */
+    public static void vistaTablero(Tablero tablero){
+        for (int i = FILA - 1; i >= 0; i--) {
+
+             if (i == FILA - 1) linea("╔", "╦", "╗", COLUMNA);
+            System.out.print("║");
+
+            for (int j = 0; j < COLUMNA; j++) {
+                Pieza t = tablero.obtenerPiezaEnCasilla(i,j);
+
+                if(t == null) System.out.printf(" %-2s ║", colorCasilla(i,j));
+                else System.out.printf(" %-2s ║", t.getDibujo());
+            }
+            System.out.println();
+
+            if (i > 0) linea("╠", "╬", "╣", COLUMNA);
+            else linea("╚", "╩", "╝", COLUMNA);
+        }
+    }
+    public static String colorCasilla(int fila, int columna) {
+        boolean color;
+
+        if(fila % 2 == 0) color = columna % 2 == 0;
+        else color = columna % 2 != 0;
+
+        if(color) return "░░";
+        else  return "▓▓";
+    }
