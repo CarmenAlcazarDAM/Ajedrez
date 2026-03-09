96ac12b9762188f1c47159c4cf91c04e8b27c305
Dani <daniguinotcar@gmail.com>
2026-03-09 13:34:17 +0100

    feat: Creacion del controlador principal

---
+++ src/main/java/controladores/ControllerPrincipal.java
@@ -0,0 +1,27 @@
+package controladores;
+
+public class ControllerPrincipal {
+    private static int contador = 1;
+
+    public boolean contadorTurnos(){
+        contador ++;
+        if (this.contador/2 == 0 ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void gestionarInicio(int opcion){
+        switch (opcion){
+            case 1 -> ControllerTablero.iniciarTablero();
+            case 2 -> //Aqui se carga una partida
+        }
+    }
+
+    public void gestionarPartida(int opcion){
+        switch (opcion){
+            case 1 ->
+        }
+    }
+}
