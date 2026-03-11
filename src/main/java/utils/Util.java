d1aa43362b1404a6e261292209f262afc373d379
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-03-11 17:25:19 +0100

    feat: metodo serializarXML() en la clase Util

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -1,7 +1,13 @@
 package utils;
 
+import modelo.Tablero;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.io.File;
 
 public  class Util {
     public static Scanner teclado = new Scanner(System.in);
@@ -28,4 +34,24 @@ public  class Util {
         } while (error);
         return n;
     }
+
+    /**
+     * Método para serializar el programa y guardar la información en un archivo XML
+     * @param tablero --> Clase Tablero pasada por parámetro
+     * @param fileName --> Nombre del archivo de destino
+     * @return --> Devuelve true si se ha guardado la información correctamente
+     */
+    public static boolean serializarXML(Tablero tablero, String fileName){
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(Tablero.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,true);
+            marshaller.marshal(tablero, new File(fileName));
+            return true;
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
