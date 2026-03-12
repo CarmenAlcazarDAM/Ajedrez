d20c0afe12e64e07cfb874d7c5ad4907064afa3b
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-03-12 12:35:48 +0100

    feat: empezar el main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,23 @@ import vista.VistaConsola;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("AJEDREZ");
+        int opcion = 0;
+
+        do{
+
+
+
+        }while (opcion!=0);
+
+
+
+
+
+
+
+
+
         //ESTO ES DE PRUEBA
 
         VistaConsola vistaConsola = new VistaConsola();
