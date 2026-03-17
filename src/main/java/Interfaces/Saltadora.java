61bb84a8dbab41e40d3cad06c1f56261dae40bf8
Carmen Alcázar <carmenmariaalcazarmartin@gmail.com>
2026-03-17 17:29:26 +0100

    feat: comentado Saltadora.java

--- src/main/java/Interfaces/Saltadora.java
+++ src/main/java/Interfaces/Saltadora.java
@@ -1,4 +1,8 @@
 package Interfaces;
 
 public interface Saltadora {
+    /**
+     * Interfaz para especificar que una pieza no compruebe si hay casillas intermedias en caso de ser Saltadora
+     * como el Caballo
+     */
 }
